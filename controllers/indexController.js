dcd5db7e18d99825859ee134bf15ec5d6a7330c8
rojasmcamilo <cristhianrojas.95@live.com>
2021-01-14 08:27:16 -0300

    escritura de archivos en proceso

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,19 +1,29 @@
 const fs = require('fs')
 const path = require('path')
 
-let task = fs.readFileSync(path.resolve(__dirname, '../data/tasks.json'), {encoding : 'utf-8'})
-task = JSON.parse(task)
-
 
 const indexController = {
     index : function(req, res, next) {
-        res.render('index', {task} );
+        res.render('index' );
       },
       agregado : (req, res) => {
-        let nuevoTask = {
-            name : req.body.name
+
+        let archivoTask = fs.readFileSync(path.resolve(__dirname, '../data/tasks.json'), {encoding : 'utf-8'});
+        let listaDeTareas;
+        if (archivoTask == "") {
+          listaDeTareas = [];
+        } else{
+          listaDeTareas = JSON.parse(archivoTask);
         }
-        fs.writeFileSync('nuevoTask', task)
+
+        let nuevoTask = {
+          name : req.body.title
+        }
+
+        //guardar al usuario
+        listaDeTareas.push(nuevoTask);
+        listaDeTareasJSON = JSON.stringify(listaDeTareas)
+        fs.appendFileSync('tasks.json', listaDeTareasJSON)
 
 
           res.redirect('/')
